709bc032e50870dc2be953d0c0267576796690cf
Marcin Mennemann <marcin@mennemann.net>
2025-09-27 22:01:45 +0200

    added gitsigns stuff

--- lua/plugins/small_plugins/gitsigns.lua
+++ lua/plugins/small_plugins/gitsigns.lua
@@ -1,6 +1,29 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require("gitsigns").setup({})
+        local gitsigns = require('gitsigns')
+
+        gitsigns.setup({
+            preview_config = {
+                border = "rounded",
+                style = 'minimal',
+            }
+        })
+
+        vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk)
+
+        vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk)
+        vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk)
+
+        vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer)
+        vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer)
+
+        vim.keymap.set('v', '<leader>hs', function()
+            gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+        vim.keymap.set('v', '<leader>hr', function()
+            gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+
     end,
 }
